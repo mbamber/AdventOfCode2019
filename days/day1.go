b0c3db7fbcdb5103b6a53191c2ae21c09f29d5b6
Matthew Bamber <matthewbamber@icloud.com>
2019-12-01 20:20:06 +0000

    :up: Add solution for Day1 Part1

--- days/day1.go
+++ days/day1.go
@@ -1,5 +1,25 @@
 package days
 
-func Day1Part1(input string) (output string, err error) {
-	return "", nil
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
+
+// Day1Part1 solves Day 1, Part 1
+func Day1Part1(input []string) (output string, err error) {
+	total := 0
+	for _, val := range input {
+		i, err := strconv.Atoi(val)
+		if err != nil {
+			return "", err
+		}
+		total += calculateFuelRequirement(i)
+	}
+
+	return fmt.Sprintf("%d", total), nil
+}
+
+func calculateFuelRequirement(mass int) int {
+	return int(math.Floor(float64(mass/3))) - 2
 }
